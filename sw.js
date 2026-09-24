2cfddfbc1b9e872d7cb60b63605e65ae37d1ef42
PageGroveHQ <pagegrovehq@gmail.com>
2026-09-24 17:08:13 -0400

    Add Circuit Sentinel learning visual system

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE = "asher-arcade-v7";
+const CACHE = "asher-arcade-v8";
 const PACKS = {"cartoon-dog-heeler":["sheaf","were","between","extreme","turkey","trolley","wheat","feast","copy","astronomy","complete","envy","money","sincere","speech","kneel","tease","freeze","barley","empty"],"william-cypher":["police","promise","reply","slight","behind","child","mire","cyclone","sighing","satisfy","lightning","die","rind","thy","untie","whine","divide","decide","sign","thigh"]};
 const AUDIO = Object.entries(PACKS).flatMap(([pack,words])=>[`./audio/${pack}/poems/the-crocodile.mp3`,...words.map(word=>`./audio/${pack}/spelling/${word}.mp3`)]);
-const LOCAL = ["./","./index.html","./styles.css","./data.js","./app.js","./manifest.webmanifest","./icon.svg","./vendor/d3.min.js","./vendor/topojson-client.min.js","./vendor/states-10m.json",...AUDIO];
+const ART = ["./assets/backgrounds/circuit-lab.jpg","./assets/backgrounds/metal-panel.jpg","./assets/characters/circuit-sentinel-action.png","./assets/characters/circuit-sentinel-success.png","./assets/characters/circuit-sentinel-thinking.png","./assets/characters/professor-volt.png","./assets/ui/energy-orb.png"];
+const LOCAL = ["./","./index.html","./styles.css","./data.js","./app.js","./manifest.webmanifest","./icon.svg","./vendor/d3.min.js","./vendor/topojson-client.min.js","./vendor/states-10m.json",...ART,...AUDIO];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(LOCAL))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
